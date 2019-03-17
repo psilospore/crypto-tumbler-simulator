1a23d627fda311da625b80ba3666690c8a588ce1
psilospore <syedjafri99@gmail.com>
2019-03-16 23:00:20 -0400

    Stub of a statistics service for recommendation on deposit amounts

---
+++ scala/src/main/scala/StatisticsService.scala
@@ -0,0 +1,7 @@
+//Store stats and make recommendations
+//TODO if I choose to implement this do it in memory. Leave a TODO for persisting
+class StatisticsService {
+  def recordDepositFromUser(): Unit  = ???
+  def recordDepositToAddress(): Unit = ???
+  def stats                          = ???
+}
